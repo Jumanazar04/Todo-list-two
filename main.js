5d3eb55a0ef7f07bd471bfa6fdfe210f76c6579b
Jumanazar04 <yusupovjumanazar004@gmail.com>
2023-09-21 06:44:09 +0500

    ch

--- main.js
+++ main.js
@@ -3,33 +3,73 @@ input = document.querySelector('.entered-text'),
 btnAdd = document.querySelector('.btn1'),
 btnEdit = document.querySelector('.pencil'),
 task = document.querySelector('.tasks'),
-btnDelete = document.querySelector('.xmark');
+text = document.querySelector('.texte'),
+btnDelete = document.querySelector('.xmark1');
 
-input.addEventListener('.submit', (e) => {
-    console.log('ok');
-    e.preventDefault()
-})
-
-btnAdd.addEventListener('.click', (e) =>{
+btnAdd.addEventListener('click', (e) => {
     e.preventDefault()
     console.log('ok');
-    if (input.value.trim() != 0) {
-        const newItem = document.createElement("div");
-        newItem.classList.add('item');
-        newItem.innerHTML = `
-        <p>${input.value}</p>
-        <button class="btn">
-            <img class="pencil" src="./svg/pencil-solid.svg" alt="svg">
-        </button>
-        <button class="btn">
-            <img class="xmark" src="./svg/xmark-solid.svg" alt="svg">
-        </button>  
-        `
-        task.appendChild(newItem);
-        input.value = ''
-        console.log('ok');
-    } else {
+    if (input.value != 0) {
+                const newItem = document.createElement("div");
+                newItem.classList.add('item');
+                newItem.innerHTML = `
+                <p class="texte">${input.value}</p>
+                    <img class="pencil" src="./svg/pencil-solid.svg" alt="svg">
+                <button class="btn xmark1">
+                    <img class="xmark" src="./svg/xmark-solid.svg" alt="svg">
+                </button>  
+                `
+                task.appendChild(newItem);
+                input.value = ''
+       } else {
         alert('Please enter some text')
-    }
-    
+     }
 })
+
+btnEdit.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log('ok');
+    text.classList.add('comleted')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// input.addEventListener('.submit', (e) => {
+//     console.log('ok');
+//     e.preventDefault()
+// })
+
+// btnAdd.addEventListener('.click', (e) =>{
+//     e.preventDefault()
+//     console.log('ok');
+//     if (input.value.trim() != 0) {
+//         const newItem = document.createElement("div");
+//         newItem.classList.add('item');
+//         newItem.innerHTML = `
+//         <p>${input.value}</p>
+//         <button class="btn">
+//             <img class="pencil" src="./svg/pencil-solid.svg" alt="svg">
+//         </button>
+//         <button class="btn">
+//             <img class="xmark" src="./svg/xmark-solid.svg" alt="svg">
+//         </button>  
+//         `
+//         task.appendChild(newItem);
+//         input.value = ''
+//         console.log('ok');
+//     } else {
+//         alert('Please enter some text')
+//     }
+    
+// })
